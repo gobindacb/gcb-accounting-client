2215f7e840887c597b8d88013d0ad45ca4e9254d
gobindacb <p.gobinda.cb@gmail.com>
2022-11-10 23:20:11 +0600

    add service solve

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddService = () => {
     return (
-        <div>
+        <div className='text-center'>
             <h1 className='text-5xl'>Please Add A Service</h1>
         </div>
     );

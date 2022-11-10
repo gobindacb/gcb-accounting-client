b770b924022a337d2a38adcc0ea2c7f148245f89
gobindacb <p.gobinda.cb@gmail.com>
2022-11-10 23:22:21 +0600

    add reiew

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddService = () => {
     return (
         <div className='text-center p-6'>
-            <h1 className='text-5xl'>Please Add A Service</h1>
+            <h1 className='text-5xl font-bold text-orange-600'>Please Add A Service</h1>
         </div>
     );
 };

0051f9ca9825fd29314b758a772118d4d08c5805
gobindacb <p.gobinda.cb@gmail.com>
2022-11-10 23:21:14 +0600

    review service add

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddService = () => {
     return (
-        <div className='text-center'>
+        <div className='text-center p-6'>
             <h1 className='text-5xl'>Please Add A Service</h1>
         </div>
     );
